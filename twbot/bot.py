310fe11b8def525081eb464e7efd492f6df6c351
professorlogicx <59408087+professorlogicx@users.noreply.github.com>
2020-05-05 13:58:07 +0530

    Update bot.py

--- twbot/bot.py
+++ twbot/bot.py
@@ -1,10 +1,10 @@
 import tweepy
 import time
 
-consumer_key = "rjS5Rr0pZ3xrR7BSF9mgqGENy"
-consumer_secret = "tvio2vFusmoqpxK5q1UCn4aj5LY1HRUw1ELfRQOlhMENq3Mlb2"
-key = "1222464601700175872-7S4aSCzUks64BQECoP33dQD45iXm9A"
-secret = "ZZXVd9GubnzMhSw3UQPhkgWYl61VRvZRJTAVGgseQmIov"
+consumer_key = "XXXXXXXXXXXXXX"
+consumer_secret = "XXXXXXXXXXXXXX"
+key = "XXXXXXXXXXXXXX-7S4aSCzUks64BQECoP33dQD45iXm9A"
+secret = "XXXXXXXXXXXXXX"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
