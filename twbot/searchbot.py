eb84930c42ccdf7efcf8334ddf5b093dcabe99f1
ya-nsh <59408087+ya-nsh@users.noreply.github.com>
2020-08-31 00:49:51 +0530

    Update searchbot.py

--- twbot/searchbot.py
+++ twbot/searchbot.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = "rjS5Rr0pZ3xrR7BSF9mgqGENy"
-consumer_secret = "tvio2vFusmoqpxK5q1UCn4aj5LY1HRUw1ELfRQOlhMENq3Mlb2"
-key = "1222464601700175872-7S4aSCzUks64BQECoP33dQD45iXm9A"
-secret = "ZZXVd9GubnzMhSw3UQPhkgWYl61VRvZRJTAVGgseQmIov"
+consumer_key = "XXXXXXXXXXXXXXXXXXXXXX"
+consumer_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+key = "XXXXXXXXXXXXXXXXXXXXX-7S4aSCzUks64BQECoP33dQD45iXm9A"
+secret = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
